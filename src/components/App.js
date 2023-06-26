13126c0b21ae41e646ecc7fe2ea8cbcfa7212176
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2023-06-26 11:02:35 +0300

    Практическая работа 10

---
+++ src/components/App.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { Header } from './Header';
+import { Main } from './Main';
+import { Footer } from './Footer';
+import { PopupWithForm } from './PopupWithForm';
+import { ImagePopup } from './ImagePopup';
+import { api } from '../utils/api';
+
+
+
+function App() {
+
+	const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false);
+	const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
+	const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
+
+	const [selectedCard, setSelectedCard] = React.useState({
+		name: '',
+		img: '',
+	});
+
+	function handleCardClick(props) {
+		setSelectedCard({
+			name: props.name,
+			img: props.img,
+		});
+	}
+
+	function handleEditProfileClick() { setEditProfilePopupOpen(true); }
+	function handleEditAvatarClick() { setEditAvatarPopupOpen(true); }
+	function handleAddPlaceClick() { setAddPlacePopupOpen(true); }
+
+	function closeAllPopups() {
+		setEditProfilePopupOpen(false);
+		setAddPlacePopupOpen(false);
+		setEditAvatarPopupOpen(false);
+		setSelectedCard({
+			name: '',
+			img: ''
+		})
+	}
+
+	return (
+
+		<div className="page">
+			<div className="page__container">
+
+				<Header />
+				<Main
+					onEditProfile={handleEditProfileClick}
+					onEditAvatar={handleEditAvatarClick}
+					onAddPlace={handleAddPlaceClick}
+					onCardClick={handleCardClick}
+				/>
+				<Footer />
+
+			</div>
+
+			<PopupWithForm
+				name="editProfile"
+				title="Редактировать профиль"
+				buttonText="Сохранить"
+				isOpen={isEditProfilePopupOpen}
+				onClose={closeAllPopups}>
+
+				<input id="input-name" name="profileName" className="popup__input popup__input_type_name" type="text"
+					placeholder="Имя" required minLength="2" maxLength="40" />
+				<span id="input-name-error" className="popup__input-error"></span>
+				<input id="input-job" name="profileJob" className="popup__input popup__input_type_job" type="text"
+					placeholder="О себе" required minLength="2" maxLength="200" />
+				<span id="input-job-error" className="popup__input-error"></span>
+			</PopupWithForm>
+
+			<PopupWithForm
+				name="addCard"
+				title="Новое место"
+				buttonText="Создать"
+				isOpen={isAddPlacePopupOpen}
+				onClose={closeAllPopups}>
+
+				<input id="input-title" name="imageName" className="popup__input popup__input_type_title" type="text"
+					placeholder="Название" required minLength="2" maxLength="30" />
+				<span id="input-title-error" className="popup__input-error"></span>
+				<input id="input-img" name="imageLink" className="popup__input popup__input_type_src" type="url"
+					placeholder="Ссылка на картинку" required />
+				<span id="input-img-error" className="popup__input-error"></span>
+			</PopupWithForm>
+
+			<PopupWithForm
+				name="editAvatar"
+				title="Обновить аватар"
+				buttonText="Сохранить"
+				isOpen={isEditAvatarPopupOpen}
+				onClose={closeAllPopups}>
+
+				<input id="input-src" name="imageAvatar" className="popup__input popup__input_type_avatar" type="url"
+					placeholder="Ссылка на картинку" required />
+				<span id="input-src-error" className="popup__input-error"></span>
+			</PopupWithForm>
+
+			<PopupWithForm
+				name="deleteCard"
+				title="Вы уверены?"
+				buttonText="Да" />
+
+			<ImagePopup
+				card={selectedCard}
+				onClose={closeAllPopups} />
+
+			{/* <!-- TEMPLATE --> */}
+
+			{/* <template id="cards-element">
+				<li className="element">
+					<img src="#" alt="#" className="element__image" />
+					<button className="element__button-delete button" type="button" aria-label="Удалить карточку"></button>
+					<div className="element__description">
+						<h2 className="element__title"></h2>
+						<div className="element__container-likes">
+							<button className="element__button-like button" type="button" aria-label="Лайкнуть карточку"></button>
+							<p className="element__like-counter">0</p>
+						</div>
+					</div>
+				</li>
+			</template> */}
+
+		</div>
+
+	);
+}
+
+export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,102 @@
+const apiOptions = {
+	url: 'https://mesto.nomoreparties.co/v1/cohort-68/',
+	headers: {
+		authorization: '3c161b6c-5a5d-4642-af7d-6f12393d02c0',
+		'Content-Type': 'application/json'
+	}
+}
+
+class Api {
+	constructor(config) {
+		this._url = config.url;
+		this._headers = config.headers;
+	}
+
+	// Получение данных о пользователе с сервера
+	getUserData() {
+		return fetch(`${this._url}/users/me`, {
+			headers: this._headers,
+		})
+			.then(this._handleResponse)
+	}
+
+	// Отправка полученных данных о пользователе на сервер
+	sendUserData(userData) {
+		return fetch(`${this._url}/users/me`, {
+			method: 'PATCH',
+			headers: this._headers,
+			body: JSON.stringify({
+				name: userData.profileName,
+				about: userData.profileJob
+			})
+		})
+			.then(this._handleResponse)
+	}
+
+	// Смена аватара пользователя
+	sendAvatarData(userAvatar) {
+		return fetch(`${this._url}/users/me/avatar`, {
+			method: 'PATCH',
+			headers: this._headers,
+			body: JSON.stringify({
+				avatar: userAvatar.imageAvatar
+			})
+		})
+			.then(this._handleResponse)
+	}
+
+	// Добавление новой карточки на сервер
+	addNewCard({ name, link }) {
+		return fetch(`${this._url}/cards`, {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify({ name, link })
+		})
+			.then(this._handleResponse)
+	}
+
+	// Загрузка карточек с сервера
+	getCards() {
+		return fetch(`${this._url}/cards`, {
+			headers: this._headers,
+		})
+			.then(this._handleResponse)
+	}
+
+	// Удаление карточки
+	deleteCard(cardId) {
+		return fetch(`${this._url}/cards/${cardId}`, {
+			method: 'DELETE',
+			headers: this._headers,
+		})
+			.then(this._handleResponse)
+	}
+
+	// Постановка лайка
+	putLike(cardId) {
+		return fetch(`${this._url}/cards/${cardId}/likes`, {
+			method: 'PUT',
+			headers: this._headers,
+		})
+			.then(this._handleResponse)
+	}
+
+	// Удаление лайка
+	deleteLike(cardId) {
+		return fetch(`${this._url}/cards/${cardId}/likes`, {
+			method: 'DELETE',
+			headers: this._headers,
+		})
+			.then(this._handleResponse)
+	}
+
+	_handleResponse(res) {
+		if (res.ok) {
+			return res.json();
+		} else {
+			return Promise.reject(`Возникла ошибка: ${res.status}`)
+		}
+	}
+}
+
+export const api = new Api(apiOptions);
